ed9d2e2d461629079b8d10858502fd048187de00
leandro.i.farias <leandro.inacio.farias@gmail.com>
2023-08-19 15:28:01 -0300

    implementacao do model e repository

--- lib/repositories/cep_repository_impl.dart
+++ lib/repositories/cep_repository_impl.dart
@@ -1,5 +1,20 @@
+import 'dart:math';
+
+import 'package:dio/dio.dart';
+import '../models/endereco_model.dart';
 import './cep_repository.dart';
 
-class CepRepositoryImpl extends CepRepository {
+
+class CepRepositoryImpl implements CepRepository {
+  @override
+  Future<EnderecoModel> getCep(String cep) async {
+    try {
+      final result = await Dio().get('viacep.com.br/ws/$cep/json/');
+      return EnderecoModel.fromMap(result.data);
+    } on DioException catch (e) {
+      log(e.hashCode);
+      throw Exception("Erro ao buscar CEP");
+    }
+  }
 
 }
